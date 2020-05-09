c2c620abb0c20200ad33e675865c2c87128bf249
John Mark Ozaeta <johnmark@Johns-MBP-2.lan>
2020-05-08 18:12:55 -0700

    Moved and cleaned files

--- app/app.R
+++ app/app.R
@@ -14,6 +14,7 @@ library(readxl)
 library(janitor)
 library(readr)
 library(ggplot2)
+library(rsconnect)
 source("app_setup.R")
 
 # User interface ----
@@ -24,50 +25,74 @@ ui <- navbarPage(
     "Worldwide Happiness",
     tabPanel("Model",
              fluidPage(
-                 titlePanel("Model Title"),
+                 titlePanel("Worldwide Happiness Model"),
                  sidebarLayout(
                      sidebarPanel(
                          selectInput(
                              "var",
                              "Choose variable to explore", 
-                             choices = c("GDP Per Capita" , "Human Freedom Index" , "Gun Violence", "Wealth Inequality(Gini Index)" )
+                             choices = c("GDP Per Capita(log10)" , "Human Freedom Index" , "Gun Violence(log10)", 
+                                         "Wealth Inequality(Gini Index)", "Life Expectancy", "Fertility Rate" )
                          ),
                          br(),
                         selectInput(
                             "wrap",
                             "Facet Wrap by Region?",
                             choices = c("Yes", "No")
-                        )),
+                        ),
+                        p("Explore the various factors that affect(or don't affect) happiness in various countries around the world.")),
                 mainPanel(plotOutput("line_plot")))
              )),
-    tabPanel("Discussion",
-             titlePanel("Discussion Title"),
-             p("Tour of the modeling choices you made and 
-              an explanation of why you made them")),
+    tabPanel("Quantifying Happiness",
+             titlePanel("Data on Happiness"),
+             h3("How is Happiness Scored?"),
+             p("Happiness is something that we think about a lot. Am I happy? Are you? In this project I hoped to answer questions that
+               we may have about happiness around the world. To answer this question I had to figure out just how happy the world is. To 
+               do this I collected data from the World Happiness Report that surveyed people around the world on how happy they were with
+               their lives. The question posed to each country was: 'If you could rate your life on a 1-10 scale, 1 being the worst life you could
+               possibly have, and 10 being the best life you could possibly have. What would you rate your life?' This question was asked to
+               over 150 countries. Of those countries I searched for data that could explain why some countries are happier than others"),
+             h3("What Correlates with Happiness?"),
+             p("After exploring dozens of variables that could explain happiness in various countries, I came to many interesting and 
+               sometimes surprising conclusions. I found GDP per capita to be the best explanatory variable for variations in happiness among countries.
+               I also was unable to find a single statistic that positively correlated with happiness that didn't also correlate with GDP per capita. 
+               This begs the question: does being richer cause all other positive variables to increase or do those characteristics make a country wealthier?"),
+             h3("Does Money Buy Happiness?"),
+             p("Yes, maybe.")),
     tabPanel("About", 
              titlePanel("About"),
              h3("Project Background and Motivations"),
-             p("Hello, this is where I talk about my project."),
+             p("This project started because I hoped to be able to answer a big question with my project. I figured there could be no greater question 
+               than what makes humans around the world happy. As I looked through data about the countries og this world I hoped to find a silver bullet:
+               something clear that would make happiness make more sense. I hoped to find, if I could, one thing that above all else, made humans happy.
+               However, our world is not that simple. What made people more satisfied with their life often came in packages. Freedom came with prosperity.
+               Health came with wealth. When our expanisve world is broken into its respective parts, variations in happiness start to decline. This would 
+               suggest that the biggest reason some places are happier than others, even with the same material resources, may come from cultural differences. 
+               These would require further examination that I cannot yet provide as an amateur data scientist.."),
              h3("About Me"),
-             p("My name is ______ and I study ______. 
-             You can reach me at ______@college.harvard.edu.")))
+             p("My name is John Mark Ozaeta and I study Economics. 
+             You can reach me at jozaeta@college.harvard.edu.")))
   
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     output$line_plot <- renderPlot({
         # Generate type based on input$plot_type from ui
         data <- switch(input$var, 
-                       "GDP Per Capita" = master$gdpPC,
+                       "GDP Per Capita(log10)" = master$gdpPC,
                        "Human Freedom Index" = master$hf_score,
-                       "Gun Violence" = master$log_td,
-                       "Wealth Inequality(Gini Index)"  = master$gini_index)
+                       "Gun Violence(log10)" = master$log_td,
+                       "Wealth Inequality(Gini Index)"  = master$gini_index,
+                       "Life Expectancy" = master$life_expectancy,
+                       "Fertility Rate" = master$fertility_rate)
         wrap <- switch(input$wrap, 
-                           "Yes" = "region",
-                           "No" = "country_name")
+                           "Yes" = ggplot(master, aes(x = data, y = happiness_score, color = region)) 
+                       + geom_point() + facet_wrap("region"),
+                           "No" = ggplot(master, aes(x = data, y = happiness_score, color = region))
+                       + geom_point())
         # Draw the histogram with the specified number of bins
         
-       ggplot(master, aes(x = data, y = happiness_score, color = region)) +geom_point() + facet_wrap(wrap)+
-           labs(x = input$var, y = "Happiness Score from 0-10")
+       wrap +
+           labs(x = input$var, y = "Happiness Score from 0-10", title = paste("Effect of", input$var, "on Happiness Around the World", sep = " ")) + theme_classic()
     })
 }
 
--- app/app_setup.R
+++ app/app_setup.R
@@ -1,4 +1,12 @@
-gun_deaths <- read.csv("~/Documents/Gov 1005/Ms_3/raw-data/gun_deaths2.csv")%>%
+library(tidyverse)
+library(dplyr)
+library(readxl)
+library(janitor)
+library(readr)
+library(ggplot2)
+
+
+gun_deaths <- read.csv("raw-data/gun_deaths2.csv")%>%
   clean_names()%>%
   rename(country_name = location)%>%
   group_by(country_name)%>%
@@ -6,22 +14,22 @@ gun_deaths <- read.csv("~/Documents/Gov 1005/Ms_3/raw-data/gun_deaths2.csv")%>%
 
 
 
-education <- read.csv("~/Documents/Gov 1005/Ms_3/raw-data/education2.csv")%>%
+education <- read.csv("raw-data/education2.csv")%>%
   clean_names()
 
-cost_of_living <- read_csv("~/Documents/Gov 1005/Ms_3/raw-data/cost_of_living.csv")%>%
+cost_of_living <- read_csv("raw-data/cost_of_living.csv")%>%
   clean_names()%>%
   rename(country_name = name)%>%
   select(country_name, cost_of_living_index)
 
-happiness <- read.csv("~/Documents/Gov 1005/Ms_3/raw-data/data.csv")%>%
+happiness <- read.csv("raw-data/data.csv")%>%
   clean_names()%>%
   rename(country_name = name)
 
-gdpPerCapita <- readxl::read_xls("~/Documents/Gov 1005/Ms_3/raw-data/gdp.xls", skip = 3)%>%
+gdpPerCapita <- readxl::read_xls("raw-data/gdp.xls", skip = 3)%>%
   clean_names()
 
-urbanization <- readxl::read_xls("~/Documents/Gov 1005/Ms_3/raw-data/urbanization.xls", skip =3)%>%
+urbanization <- readxl::read_xls("raw-data/urbanization.xls", skip =3)%>%
   clean_names()%>%
   rename(urbanization = x2018)%>%
   select(country_name, urbanization)
@@ -29,7 +37,7 @@ urbanization <- readxl::read_xls("~/Documents/Gov 1005/Ms_3/raw-data/urbanizatio
 gdpPerCapita2 <- gdpPerCapita%>%
   select(country_name, country_code, x2018)
 
-HFI <- read.csv("~/Documents/Gov 1005/Ms_3/raw-data/hfi_2018.csv")%>%
+HFI <- read.csv("raw-data/hfi_2018.csv")%>%
   clean_names()%>%
   mutate(myyear = max(year))%>%
   filter(year == myyear)%>%
@@ -37,7 +45,7 @@ HFI <- read.csv("~/Documents/Gov 1005/Ms_3/raw-data/hfi_2018.csv")%>%
   rename(country_code = iso_code)
 
 
-gini <- read.csv("~/Documents/Gov 1005/Ms_3/raw-data/gini.csv")%>%
+gini <- read.csv("raw-data/gini.csv")%>%
   clean_names()
 
 clean_gini <- gini%>%
@@ -51,7 +59,7 @@ clean_gini <- gini%>%
 
 #chrstgenpct, judgenpct, islmgenpct, budgenpct, zorogenpct, hindgenpct, #sikhgenpct, shntgenpct, )
 
-religion <- readxl::read_xlsx("~/Documents/Gov 1005/Ms_3/raw-data/Religious_Composition_by_Country_2010-2050.xlsx", sheet =   "rounded_percentage")%>%
+religion <- readxl::read_xlsx("raw-data/Religious_Composition_by_Country_2010-2050.xlsx", sheet =   "rounded_percentage")%>%
   filter(Year == 2010)%>%
   select(Country, Unaffiliated)%>%
   mutate(nonreligpct = as.numeric(Unaffiliated))%>%
@@ -64,7 +72,7 @@ master1 <- happiness%>%
   full_join(gdpPerCapita2)
 
 
-cleaned <- read_csv("~/Documents/Gov 1005/Ms_3/raw-data/troubleshoot1.csv")
+cleaned <- read_csv("raw-data/troubleshoot1.csv")
 
 cleaned1 <- cleaned%>%
   filter(is.na(x2018))%>%
@@ -72,6 +80,16 @@ cleaned1 <- cleaned%>%
   left_join(gdpPerCapita2, by = "country_code")%>%
   select(country_name.x, country_code, x2018)%>%
   rename(country_name = country_name.x)
+
+fertility_rate <- read.csv("raw-data/fertility.csv", skip =3)%>%
+  clean_names()%>%
+  select(country_code, x2018)%>%
+  rename(fertility_rate = x2018)
+
+life_expectancy <- read.csv("raw-data/expectancy.csv", skip =3)%>%
+  clean_names()%>%
+  select(country_code, x2018)%>%
+  rename(life_expectancy = x2018)
 
 master <- cleaned%>%
   filter(!is.na(x2018))%>%
@@ -95,4 +113,10 @@ master <- master%>%
 
 master <- master%>%
   mutate(gdpPC = log10(x2018), log_td = log10(total_deaths))
-         
+
+master <- master%>%
+  left_join(fertility_rate, by = "country_code")
+
+master <- master%>%
+  left_join(life_expectancy, by = "country_code")
+       
